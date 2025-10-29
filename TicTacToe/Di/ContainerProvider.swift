16d72c8bea7be5b60552db7e0c5e4c25754d84ab
iva-a-a <ivanova.alena.a98@gmail.com>
2025-10-29 22:34:25 +0200

    добавила зависимости в ContainerProvider

--- TicTacToe/Di/ContainerProvider.swift
+++ TicTacToe/Di/ContainerProvider.swift
@@ -18,6 +18,7 @@ final class ContainerProvider {
         registerNetwork()
         registerRepositories()
         registerServices()
+        registerErrorHandling()
     }
     
     private func registerCoreData() {
@@ -33,43 +34,84 @@ final class ContainerProvider {
             resolver.resolve(CoreDataStack.self)!.bg()
         }
     }
-    
-    private func registerNetwork() {
-        container.register(NetworkClient.self) { _ in
-            NetworkClient.shared
-        }.inObjectScope(.container)
 
+    private func registerNetwork() {
         container.register(ApiServiceProtocol.self) { resolver in
             ApiService(client: resolver.resolve(NetworkClient.self)!)
         }.inObjectScope(.container)
+
+        container.register(RequestInterceptor.self) { resolver in
+            let tokenRepo = resolver.resolve(TokenRepositoryProtocol.self)!
+            let apiServiceProvider: () -> ApiServiceProtocol = {
+                resolver.resolve(ApiServiceProtocol.self)!
+            }
+            return AuthRequestInterceptor(
+                tokenRepository: tokenRepo,
+                apiServiceProvider: apiServiceProvider
+            )
+        }.inObjectScope(.container)
+
+        container.register(NetworkClient.self) { resolver in
+            let interceptor = resolver.resolve(RequestInterceptor.self)
+            return NetworkClient(interceptor: interceptor)
+        }.inObjectScope(.container)
     }
-    
+
     private func registerRepositories() {
         container.register(CurrentUserRepositoryProtocol.self) { resolver in
             CurrentUserRepository(context: resolver.resolve(NSManagedObjectContext.self, name: "bgContext")!)
-        }
+        }.inObjectScope(.container)
 
         container.register(UserRepositoryProtocol.self) { resolver in
             UserRepository(context: resolver.resolve(NSManagedObjectContext.self, name: "bgContext")!)
-        }
+        }.inObjectScope(.container)
 
         container.register(GameRepositoryProtocol.self) { resolver in
             GameRepository(context: resolver.resolve(NSManagedObjectContext.self, name: "bgContext")!)
-        }
+        }.inObjectScope(.container)
+        
+        container.register(LeaderboardRepositoryProtocol.self) { resolver in
+            LeaderboardRepository(context: resolver.resolve(NSManagedObjectContext.self, name: "bgContext")!)
+        }.inObjectScope(.container)
+        
+        container.register(TokenRepositoryProtocol.self) { _ in
+            TokenRepository.shared
+        }.inObjectScope(.container)
+
     }
-    
+
     private func registerServices() {
         container.register(CurrentUserServiceProtocol.self) { resolver in
             CurrentUserService(currentUserRepository: resolver.resolve(CurrentUserRepositoryProtocol.self)!)
-        }
+        }.inObjectScope(.container)
 
         container.register(UserServiceProtocol.self) { resolver in
             UserService(userRepository: resolver.resolve(UserRepositoryProtocol.self)!)
-        }
+        }.inObjectScope(.container)
 
         container.register(GameServiceProtocol.self) { resolver in
             GameService(gameRepository: resolver.resolve(GameRepositoryProtocol.self)!)
-        }
+        }.inObjectScope(.container)
+
+        container.register(LeaderboardServiceProtocol.self) { resolver in
+            LeaderboardService(leaderboardRepository: resolver.resolve(LeaderboardRepositoryProtocol.self)!)
+        }.inObjectScope(.container)
+
+        container.register(SessionServiceProtocol.self) { resolver in
+            SessionService(
+                currentUserService: resolver.resolve(CurrentUserServiceProtocol.self)!,
+                gameService: resolver.resolve(GameServiceProtocol.self)!,
+                userService: resolver.resolve(UserServiceProtocol.self)!,
+                leaderboardService: resolver.resolve(LeaderboardServiceProtocol.self)!,
+                tokenRepository: resolver.resolve(TokenRepositoryProtocol.self)!
+            )
+        }.inObjectScope(.container)
+    }
+    
+    private func registerErrorHandling() {
+        container.register(ApiErrorHandlingStrategy.self) { resolver in
+            ApiErrorHandlingStrategy(sessionService: resolver.resolve(SessionServiceProtocol.self)!)
+        }.inObjectScope(.container)
     }
 
     func resolve<T>(_ type: T.Type) -> T {
@@ -79,3 +121,4 @@ final class ContainerProvider {
         return dependency
     }
 }
+
