dad2c0cf46b1da8b48fab5a0b389755abafcefda
iva-a-a <ivanova.alena.a98@gmail.com>
2025-10-29 22:25:32 +0200

    переписала AuthInterceptor с учетом токенов

--- TicTacToe/Data/Network/Api/AuthInterceptor.swift
+++ TicTacToe/Data/Network/Api/AuthInterceptor.swift
@@ -3,28 +3,10 @@
 //  TicTacToe
 
 import Foundation
-import CoreData
 
 struct AuthInterceptor {
-    static func authHeader() -> String? {
-        let context = CoreDataStack.shared.viewContext
-        let request: NSFetchRequest<CurrentUserEntity> = CurrentUserEntity.fetchRequest()
-        
-        do {
-            guard let user = try context.fetch(request).first,
-                  let login = user.login,
-                  let password = user.password,
-                  !login.isEmpty,
-                  !password.isEmpty
-            else { return nil }
-            
-            let loginPassword = "\(login):\(password)"
-            guard let data = loginPassword.data(using: .utf8) else { return nil }
-            
-            return "Basic \(data.base64EncodedString())"
-        } catch {
-            print("AuthInterceptor fetch error: \(error)")
-            return nil
-        }
+    static func authHeader(tokenRepository: TokenRepositoryProtocol = TokenRepository.shared) -> String? {
+        guard let accessToken = tokenRepository.accessToken else { return nil }
+        return "Bearer \(accessToken)"
     }
 }
